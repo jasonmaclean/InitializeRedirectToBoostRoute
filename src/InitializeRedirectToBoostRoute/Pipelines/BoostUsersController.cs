10f0934f562fd0cc89211f424fe778ebb0ff7699
Jason MacLean <Jason.MacLean@mnp.ca>
2024-09-07 16:52:50 -0400

    initial commit

---
+++ src/InitializeRedirectToBoostRoute/Pipelines/BoostUsersController.cs
@@ -0,0 +1,66 @@
+﻿using Sitecore.Diagnostics;
+using Sitecore.Security.Accounts;
+using Sitecore.Text;
+using Sitecore.Web;
+using System.Web.Mvc;
+
+namespace Sitecore.Support.Client.LicenseOptions.Controllers
+{
+    public class BoostUsersController : Controller
+    {
+        [HttpGet]
+        public void RedirectToBoost()
+        {
+            if (Context.User.IsAuthenticated || !Context.User.IsAdministrator)
+            {
+                Log.Info($"User {Context.User.Name} is being presented a 401 response.", this);
+
+                base.Response.StatusCode = 401;
+            }
+            else
+            {
+                var boostUrl = GetBoostUrl();
+
+                Log.Info($"User {Context.User.Name} is being redirected to {boostUrl}.", this);
+
+                base.Response.Redirect(boostUrl, endResponse: true);
+            }
+        }
+
+        protected string GetBoostUrl()
+        {
+            var licenseID = SecurityModel.License.License.LicenseID;
+            var licensee = SecurityModel.License.License.Licensee;
+
+            var urlString = new UrlString(WebUtil.GetFullUrl(new StartUrlManager().GetStartUrl(Context.User)));
+            urlString.Add("inv", "1");
+
+            var urlString2 = new UrlString("http://www.sitecore.net/boost");
+            urlString2.Add("url", urlString.ToString());
+
+            urlString2.Add("lid", StringUtil.GetString(new string[]
+            {
+                licenseID
+            }));
+
+            urlString2.Add("host", StringUtil.GetString(new string[]
+            {
+                WebUtil.GetHostName()
+            }));
+
+            urlString2.Add("ip", StringUtil.GetString(new string[]
+            {
+                WebUtil.GetHostIPAddress()
+            }));
+            urlString2.Add("licensee", StringUtil.GetString(new string[]
+            {
+                licensee
+            }));
+            urlString2.Add("iisname", StringUtil.GetString(new string[]
+            {
+                WebUtil.GetIISName()
+            }));
+            return urlString2.ToString();
+        }
+    }
+}
---
+++ src/InitializeRedirectToBoostRoute/Pipelines/InitializeRedirectToBoostRoute.cs
@@ -0,0 +1,33 @@
+﻿using Sitecore.Diagnostics;
+using Sitecore.Pipelines;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace SitecoreFundamentals.InitializeRedirectToBoostRoute.Pipelines
+{
+    public class InitializeRedirectToBoostRoute
+    {
+    }
+}
+namespace Sitecore.Support.Mvc.Pipelines.Initialize
+{
+    internal class InitializeRedirectToBoostRoute
+    {
+        public virtual void Process(PipelineArgs args)
+        {
+            Assert.ArgumentNotNull(args, "args");
+            this.RegisterRoutes(RouteTable.Routes, args);
+        }
+
+        protected virtual void RegisterRoutes(RouteCollection routes, PipelineArgs args)
+        {
+            string[] namespaces = new string[] { "Sitecore.Support.Client.LicenseOptions.Controllers" };
+            routes.MapRoute("RouteName", "api/sitecore/BoostUsers/{action}", new
+            {
+                controller = "BoostUsers",
+                action = "RedirectToBoost",
+                id = UrlParameter.Optional
+            }, namespaces);
+        }
+    }
+}
